48271ed3bbfec0402ed973760007d3f12f0da4ab
sampaw13 <samarielpaw@gmail.com>
2018-07-23 18:51:22 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -18,7 +18,7 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts lowercased_phrase
+puts #IM NOT SHOUTING.downcase
 
 # Challenge 4: Count how many letters are in that big_word
 
@@ -26,7 +26,7 @@ big_word = "supercalifragilisticexpialidocious"
 
 letter_count = big_word # Put a . after big_word and use a String method.
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are #{letter_count} in #supercalifragilisticexpialidocious"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
